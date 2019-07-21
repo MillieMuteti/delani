1e24930cf76ab53d07be2cbacb0f92cf68fa02d7
Millie Muteti <milliemutetigmail.com>
2019-07-21 01:59:14 -0400

    Make changes on portfolio section so that the hovering effect works

--- js/scripts.js
+++ js/scripts.js
@@ -11,16 +11,51 @@ $(document).ready(function(){
         $(".dev-description").fadeToggle();
         $(".dev").fadeToggle();
     });
-    $("#project").hover(function(){
+    $("#empty, #project").hover(function(){
         $("#empty").slideDown("slow");
         $("#project").mouseout(function(){
             $("#empty").slideUp("slow");
         });
     });
-    $("#project1").hover(function(){
+    $("#pic1, #project2").hover(function(){
         $("#pic1").slideDown("slow");
         $("#project2").mouseout(function(){
-            $("#pic1").slideUp("slow");
+            $("pic1").slideUp("slow");
         });
     });
-});
+    });
+    $("#pic2, #project3").hover(function(){
+        $("#pic2").slideDown("slow");
+        $("#project3").mouseout(function(){
+            $("#pic2").slideUp("slow");
+        }); 
+        });$("#pic3, #project4").hover(function(){
+            $("#pic3").slideDown("slow");
+            $("#project4").mouseout(function(){
+                $("#pic3").slideUp("slow");
+            });
+        });
+        $("#pic4, #project5").hover(function(){
+            $("#pic4").slideDown("slow");
+            $("#project5").mouseout(function(){
+                $("#pic4").slideUp("slow");
+            });
+        });
+        $("#pic5, #project6").hover(function(){
+            $("#pic5").slideDown("slow");
+            $("#project6").mouseout(function(){
+                $("#pic5").slideUp("slow");
+            });
+        });
+        $("#pic6, #project7").hover(function(){
+            $("#pic6").slideDown("slow");
+            $("#project7").mouseout(function(){
+                $("#pic6").slideUp("slow");
+            });
+        });
+        $("#pic7, #project8").hover(function(){
+        $("#pic7").slideDown("slow");
+        $("#project8").mouseout(function(){
+            $("#pic7").slideUp("slow");
+        });
+        });
